7957684f8f1112e8203b55e188541f50142b9a2c
Мира <myroslava.yaremenko@nure.ua>
2016-12-04 16:17:35 +0200

    infinite scrolling added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,29 +14,45 @@ class App extends React.Component {
             limit: 25
         };
         this.loadData = this.loadData.bind(this);
+        this.scrollHandler = this.scrollHandler.bind(this);
     }
 
     componentDidMount() {
         this.loadData();
+        window.addEventListener('scroll', this.scrollHandler);
     };
 
     componentWillUnmount() {
         this.serverRequest.abort();
+        window.removeEventListener('scroll', this.scrollHandler);
     }
 
     loadData() {
-
         let _this = this;
         this.serverRequest =
             axios.get(`http://pokeapi.co/api/v2/pokemon/?limit=${this.state.limit}&offset=${this.state.offset}`)
                 .then((result) => {
-
                     _this.setState({
                         pokemons: this.state.pokemons.concat(result.data.results),
                         offset: this.state.offset + this.state.limit
                     });
-
                 });
+    }
+
+    scrollHandler() {
+
+        const html = document.documentElement,
+            body = document.body,
+            scrollTop = html.scrollTop || body && body.scrollTop || 0;
+        let inProgress = false;
+
+        if(html.clientHeight + scrollTop >= html.scrollHeight &&! inProgress){
+            this.loadData();
+            inProgress = true;
+        }
+        console.log(scrollTop);
+
+
     }
 
     render() {
@@ -44,7 +60,6 @@ class App extends React.Component {
         return (
             <div className="App">
                 <h1>Pokemons App</h1>
-                <button onClick={this.loadData}>add</button>
                 {this.state.pokemons.map((pokemon) => {
                     return <Pokemon key={pokemon.url} pName={pokemon.name} pUrl={pokemon.url}/>;
                 })}
